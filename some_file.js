dba5e16dfa7b5a207d5f6ea4e4d9e70495216b94
Rygielski <rygielsp@emea.roche.com>
2016-02-07 12:44:19 +0100

    some changes

--- some_file.js
+++ some_file.js
@@ -1,44 +1,31 @@
 
 function test4(){
+
   console.log('test5');
+
 }
+function testNext(){}
 function test1(){
-
   console.log('TEST1');
-
   if ( true ) {
-
     console.log( 'TRUE');
-
   }
-
-
 }
 
 
-function test3(){
-
-  console.log('TEST3');
-
-  if ( true ){
-
-    console.log('TEST3');
-
-  }
-
-}
 
 
 function test2(){
-
   console.log('TEST2');
-
   if ( true ){
-
     console.log('TEST2`');
-
   }
-
+}
+function test3(){
+  console.log('TEST3');
+  if ( true ){
+    console.log('TEST3');
+  }
 }
 
 function test5(){
